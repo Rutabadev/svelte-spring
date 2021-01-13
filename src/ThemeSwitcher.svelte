3a627a93e22786d897243df7847b59f97ae68ac8
etienne.raby <etienne.raby@fittingbox.com>
2021-01-13 21:30:23 +0100

    ADD: ThemeSwitcher remember choice in local storage & reset it w/ right click

--- src/ThemeSwitcher.svelte
+++ src/ThemeSwitcher.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
 import {darkTheme} from './stores'
+
+const resetTheme = () => {
+    if (confirm('Do you want to reset the theme based on your system preference ?')) {
+        darkTheme.reset()
+    }
+}
 </script>
 
-<label for="theme-switcher">
-    <input checked={$darkTheme} on:change={darkTheme.toggle} id="theme-switcher" type="checkbox">
+<label for="theme-switcher" on:contextmenu|preventDefault={resetTheme}>
+    <input checked={$darkTheme} on:change={darkTheme.toggle} on:contextmenu|preventDefault={resetTheme} id="theme-switcher" type="checkbox">
     Dark theme
 </label>
 
--- src/stores.ts
+++ src/stores.ts
@@ -2,21 +2,30 @@ import {writable} from 'svelte/store'
 
 const createDarkThemeStore = () => {
 
-    const setDarkTheme = (dark) => {
+    const setDarkTheme = (dark: boolean) => {
         const func = dark ? 'add' : 'remove'
         document.body.classList[func]('dark')
     }
-    const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches
+    const prefersDark = () => window.matchMedia('(prefers-color-scheme: dark)').matches
+    const isDark = localStorage.theme === 'dark' || (!localStorage.theme) && prefersDark()
+
     setDarkTheme(isDark)
-    const { subscribe, update } = writable(isDark)
+    const { subscribe, update, set } = writable(isDark)
     return {
         subscribe,
-
-        toggle: () => update(val => {
+        toggle: () => update((val: boolean) => {
             const newVal = !val
             setDarkTheme(newVal)
+            localStorage.setItem('theme', newVal ? 'dark' : 'light')
             return newVal
-        })
+        }),
+        reset: () => {
+            if (localStorage.theme) {
+                localStorage.removeItem('theme')
+            }
+            setDarkTheme(prefersDark())
+            set(prefersDark())
+        }
     }
 }
 
