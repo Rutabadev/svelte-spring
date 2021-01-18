3b7d324d368817805b325b648cd4ebab40349b62
etienne.raby <etienne.raby@fittingbox.com>
2021-01-19 00:47:01 +0100

    reverse theme switcher

--- src/ThemeSwitcher.svelte
+++ src/ThemeSwitcher.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-   import { darkTheme } from './stores';
+   import { darkTheme, mute } from './stores';
 
    const resetTheme = () => {
       if (
@@ -12,8 +12,10 @@
    };
 
    const handleClick = () => {
-      const audio = new Audio('/light-switch.mp3');
-      audio.play();
+      if (!$mute) {
+         const audio = new Audio('/light-switch.mp3');
+         audio.play();
+      }
       darkTheme.toggle();
    };
 </script>
@@ -29,9 +31,7 @@
          viewBox="0 0 20 20"
          fill="currentColor">
          <path
-            fill-rule="evenodd"
-            d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
-            clip-rule="evenodd"
+            d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"
          />
       </svg>
    {:else}
@@ -40,7 +40,9 @@
          viewBox="0 0 20 20"
          fill="currentColor">
          <path
-            d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"
+            fill-rule="evenodd"
+            d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+            clip-rule="evenodd"
          />
       </svg>
    {/if}
@@ -48,6 +50,7 @@
 
 <style>
    .switch {
+      transform: translate3d(0, -1px, 0);
       padding: 0.5rem;
       width: 2rem;
       height: 2rem;
