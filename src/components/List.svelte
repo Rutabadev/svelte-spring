689e3daa09f1df2703ae7cb82f883f2075ad4838
etienne.raby <etienne.raby@fittingbox.com>
2021-04-13 22:45:10 +0200

    fun with context

---
+++ src/components/List.svelte
@@ -0,0 +1,28 @@
+<script context="module">
+   export const key = {};
+</script>
+
+<script lang="ts">
+   import { writable } from 'svelte/store';
+   import { setContext } from 'svelte';
+
+   let currentColorId = 0;
+   const colors = ['rebeccapurple', 'blue', 'red', 'gold'];
+   let color = writable(colors[currentColorId]);
+   setContext(key, { getColor: () => color });
+</script>
+
+<ul
+   on:click={() => {
+      currentColorId = (currentColorId + 1) % colors.length;
+      $color = colors[currentColorId];
+   }}
+>
+   <slot />
+</ul>
+
+<style>
+   ul {
+      cursor: pointer;
+   }
+</style>
---
+++ src/partials/Context.svelte
@@ -0,0 +1,35 @@
+<script context="module">
+   export const key = {};
+</script>
+
+<script lang="ts">
+   import { setContext } from 'svelte';
+   import { writable } from 'svelte/store';
+
+   import List from '../components/List.svelte';
+   import ListItem from '../components/ListItem.svelte';
+
+   const text = writable('item');
+   setContext(key, text);
+</script>
+
+<div
+   on:mouseenter={() => {
+      $text = 'hovered';
+   }}
+   on:mouseleave={() => {
+      $text = 'item';
+   }}
+>
+   <List>
+      {#each Array(4) as _item, i}
+         <ListItem>{$text} {i + 1}</ListItem>
+      {/each}
+   </List>
+</div>
+
+<List>
+   {#each Array(4) as _item, i}
+      <ListItem>{$text === 'hovered' ? 'not ' : ''}{$text} {i + 1}</ListItem>
+   {/each}
+</List>
