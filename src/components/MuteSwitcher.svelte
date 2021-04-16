4cc26af1bde4c84b0fc957e7d8bcb0ca6a896cd5
etienne.raby <etienne.raby@fittingbox.com>
2021-04-16 21:50:35 +0200

    svg optimizations

--- src/components/MuteSwitcher.svelte
+++ src/components/MuteSwitcher.svelte
@@ -20,13 +20,13 @@
 </script>
 
 <Switch on:click={handleClick} on:contextmenu={resetSound}>
-   {#if $mute}
-      <svg
-         xmlns="http://www.w3.org/2000/svg"
-         fill="none"
-         viewBox="0 0 24 24"
-         stroke="currentColor"
-      >
+   <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+   >
+      {#if $mute}
          <path
             stroke-linecap="round"
             stroke-linejoin="round"
@@ -40,20 +40,13 @@
             stroke-width="2"
             d="M17 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2"
          />
-      </svg>
-   {:else}
-      <svg
-         xmlns="http://www.w3.org/2000/svg"
-         fill="none"
-         viewBox="0 0 24 24"
-         stroke="currentColor"
-      >
+      {:else}
          <path
             stroke-linecap="round"
             stroke-linejoin="round"
             stroke-width="2"
             d="M15.536 8.464a5 5 0 010 7.072m2.828-9.9a9 9 0 010 12.728M5.586 15H4a1 1 0 01-1-1v-4a1 1 0 011-1h1.586l4.707-4.707C10.923 3.663 12 4.109 12 5v14c0 .891-1.077 1.337-1.707.707L5.586 15z"
          />
-      </svg>
-   {/if}
+      {/if}
+   </svg>
 </Switch>
